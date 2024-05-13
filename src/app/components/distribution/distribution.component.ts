f2bdb0a28fb95aad2cea3ac4707b7508d62e80c0
Arsen4ik <arsenfazleev@gmail.com>
2024-05-13 19:13:45 +0300

    hz3

---
+++ src/app/components/distribution/distribution.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'distribution',
+  templateUrl: './distribution.component.html',
+  styleUrl: './distribution.component.scss'
+})
+export class DistributionComponent {
+
+}
