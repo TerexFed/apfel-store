d2179a1fdd5e681b7a715d0526ae24bd77cef572
TerexFed <terexfed9@gmail.com>
2024-05-27 18:25:31 +0300

    contact page layout

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalWindowComponent } from '../../UI/modal-window/modal-window.component';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
+  constructor(public dialog: MatDialog) { }
 
+  openModalCallback() {
+    window.scrollTo(0, 0)
+    this.dialog.open(ModalWindowComponent, { data: { type: 'CallBack'} })
+  }
 }
