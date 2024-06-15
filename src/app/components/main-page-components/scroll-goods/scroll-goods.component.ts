44a5569bd615b8d41521c5eb663f0441ee739c43
terexfed <terexfed9@gmail.com>
2024-06-15 12:54:51 +0300

    scroll first version

--- src/app/components/main-page-components/scroll-goods/scroll-goods.component.ts
+++ src/app/components/main-page-components/scroll-goods/scroll-goods.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './scroll-goods.component.scss'
 })
 export class ScrollGoodsComponent {
+  public scrollGoods: Array<any> = [
+    { id: 0, image: '../../../../assets/main-page/scroll-goods/big_iphone.svg', price: 137900 },
+    { id: 1, image: '../../../../assets/main-page/scroll-goods/ipad.svg', price: 137900, text: 'Ipad Pro 11' },
+    { id: 2, image: '../../../../assets/main-page/scroll-goods/macbook.svg', price: 137900, text: 'MackBook Pro' }
+  ]
 
+  public currentId: number = 0
+
+
+  public scrollRight() {
+    this.currentId = (this.currentId + 1) % this.scrollGoods.length;
+  }
+
+  public scrollLeft() {
+    this.currentId = (this.currentId - 1 + this.scrollGoods.length) % this.scrollGoods.length;
+  }
 }
